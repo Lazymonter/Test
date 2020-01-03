1271597b6779758237c15ea203c27706215bc96d
Lazymonter <shx_929@163.com>
2020-01-03 20:52:36 +0800

    change catalogue

--- rank/QuickSort.cpp
+++ rank/QuickSort.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-void print(int * A)
+void print(vector<int> &result)
 {
-    for(int i = 0;i < 10;i++)
-    {
-        cout << A[i] << " ";
+    for(vector<int>::const_iterator iter = result.begin(); iter != result.end();iter++){
+        cout << *iter << " ";
     }
     cout << endl;
 }
-void Exchange(int * A,int i,int j)
+void Exchange(vector<int> &result,int i,int j)
 {
-    if(A != NULL)
-    {
-      int temp = A[i];
-      A[i] = A[j];
-      A[j] = temp;
-    }
+    int temp = result[i];
+    result[i] = result[j];
+    result[j] = temp;
 
 }
-int Partition(int * A,int p,int r)
+int Partition(vector<int> &A,int p,int r)
 {
     int x = A[r];
     int i = p - 1;
@@ -39,25 +36,25 @@ int Partition(int * A,int p,int r)
     return i + 1;
 
 }
-void QuickSort(int * A,int p,int r)
+void QuickSort(vector<int> &A,int p,int r)
 {
     int q = 0;
-   if(p < r)
-   {
-       q = Partition(A,p,r);
-       QuickSort(A,p,q - 1);
-       QuickSort(A,q + 1,r);
-   }
+    if(p < r)
+    {
+        q = Partition(A,p,r);
+        QuickSort(A,p,q - 1);
+        QuickSort(A,q + 1,r);
+    }
 }
 
 int main()
 {
-    int A[10] = {22,4,5,3,1,25,67,9,8,23};
+    vector<int> A = {22,4,5,3,1,25,67,9,8,23};
     QuickSort(A,0,9);
     for(int i = 0;i < 10;i++)
     {
        cout << A[i] << " ";
     }
-    cout << "\n" << "Hello world!" << endl;
+    cout << endl;
     return 0;
 }
